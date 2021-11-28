dde559fe6fe853d1680c95a2c814990d6a887c5a
idilettant <dedenev.dev@gmail.com>
2021-11-28 19:46:06 +0300

    Create main app init module

---
+++ paymaster/main.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from paymaster.api_router import router
+from paymaster.events import create_start_app_handler, create_stop_app_handler
+
+load_dotenv()
+
+
+def get_application() -> FastAPI:
+    application = FastAPI()
+    application.add_event_handler(
+        "startup",
+        create_start_app_handler(application),
+    )
+    application.add_event_handler(
+        "shutdown",
+        create_stop_app_handler(application),
+    )
+    application.include_router(router)
+    return application
+
+
+app = get_application()
