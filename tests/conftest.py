f732f5230976b653c41279c85ba82509286de261
idilettant <dedenev.dev@gmail.com>
2021-11-28 19:29:40 +0300

    Update fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,38 @@
 """Fixtures module."""
-import asyncpg
+import os
+
 import pytest
+from asgi_lifespan import LifespanManager
+from asyncpg import Pool
+from fastapi import FastAPI
+from httpx import AsyncClient
+from testcontainers.postgres import PostgresContainer
 
-DSN = 'postgresql://postgres:postgres@localhost:5432'
+
+@pytest.fixture
+async def dsn() -> Pool:
+    with PostgresContainer("postgres:12") as postgres:
+        yield postgres.get_connection_url().replace('+psycopg2', '')  # TODO: write comment about replacement
 
 
-@pytest.fixture()
-async def db_conn():
-    connection = await asyncpg.connect(dsn=DSN)
-    transaction = connection.transaction()
-    await transaction.start()
-    yield connection
-    await transaction.rollback()
-    await connection.close()
+@pytest.fixture
+async def app() -> FastAPI:
+    from paymaster.main import get_application  # local import for testing purpose
+
+    yield get_application()
+
+
+@pytest.fixture
+async def initialized_app(app: FastAPI, dsn: str) -> FastAPI:
+    os.environ['DSN'] = dsn
+    async with LifespanManager(app):
+        yield app
+
+
+@pytest.fixture
+async def client(initialized_app: FastAPI) -> AsyncClient:
+    async with AsyncClient(
+        app=initialized_app,
+        base_url="http://testserver",
+    ) as client:
+        yield client
