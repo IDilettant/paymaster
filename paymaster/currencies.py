5c4adffe88b534fdde73c47fdd1aec681e46519d
idilettant <dedenev.dev@gmail.com>
2021-11-29 16:42:18 +0300

    Update with httpx

--- paymaster/currencies.py
+++ paymaster/currencies.py
@@ -1,6 +1,5 @@
 from typing import Any, List, Tuple
-
-import aiohttp
+import httpx
 
 BASE_CURRENCY = 'rub'
 
@@ -10,7 +9,9 @@ async def get_currencies_rates(
         base_currency: str = BASE_CURRENCY,
 ) -> List[Tuple[Any]]:
     url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/{base_currency}'
-    async with aiohttp.request(method='get', url=url, raise_for_status=True) as resp:
-        response = await resp.json()
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        resp.raise_for_status()
+        response = resp.json()
         cur_rates = response['conversion_rates']
         return [tuple([currency, cur_rates[currency]]) for currency in cur_rates]
