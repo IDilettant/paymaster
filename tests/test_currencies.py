0a816f40f21a09e93858d2f44a00ffca68c893e8
idilettant <dedenev.dev@gmail.com>
2021-11-29 16:41:21 +0300

    Add test

--- tests/test_currencies.py
+++ tests/test_currencies.py
@@ -1,8 +1,23 @@
-from paymaster.app import API_KEY
-from paymaster.currencies import get_currencies_rates, BASE_CURRENCY
+import pytest
+from pytest_httpx import HTTPXMock
+import httpx
+
+from paymaster.currencies import BASE_CURRENCY, get_currencies_rates
+
+pytestmark = pytest.mark.asyncio
 
 
-async def test_get_currencies_rates():
-    cur_rate = await get_currencies_rates(API_KEY)
-    assert cur_rate[0][0] == BASE_CURRENCY
+async def test_get_currencies_rates(httpx_mock: HTTPXMock):
+    def custom_response(request: httpx.Request, *args, **kwargs):
+        data = {'result': 'success', 'base_code': 'RUB', 'conversion_rates': {BASE_CURRENCY.upper(): 1, 'USD': 0.01326}}
+        return httpx.Response(
+            status_code=200, json=data,
+        )
+
+    httpx_mock.add_callback(custom_response)
+    api_key = 'some_api_key'
+    cur_rate = await get_currencies_rates(api_key=api_key)
+    assert cur_rate[0][0] == BASE_CURRENCY.upper()
     assert cur_rate[0][1] == 1
+    assert cur_rate[1][0] == 'USD'
+    assert cur_rate[1][1] == 0.01326
