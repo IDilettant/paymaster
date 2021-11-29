d1eee2a8449e1c8833083d993d0ae13cee28b023
idilettant <dedenev.dev@gmail.com>
2021-11-29 21:24:28 +0300

    Add tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,14 +3,77 @@ import pytest
 from fastapi import status
 from httpx import AsyncClient
 
-pytestmark = pytest.mark.asyncio
+from paymaster.data_schemas import OperationType
 
-first_user_id = 444
-second_user_id = 555
+pytestmark = pytest.mark.asyncio
 
 
 async def test_app(client: AsyncClient):
+    first_user_id = 444
+    second_user_id = 555
+
     response = await client.post(f'/account/create/user_id/{first_user_id}')
     assert response.status_code == status.HTTP_201_CREATED
+
     response = await client.post(f'/account/create/user_id/{first_user_id}')
     assert response.status_code == status.HTTP_409_CONFLICT
+
+    response = await client.post(f'/account/delete/user_id/{first_user_id}')
+    assert response.status_code == status.HTTP_205_RESET_CONTENT
+
+    await client.post(f'/account/create/user_id/{first_user_id}')
+    response = await client.post(
+        '/balance/change',
+        json={
+            'operation': OperationType.credit,
+            'user_id': first_user_id,
+            'total': 100,
+            'description': 'replenishment',
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+    response = await client.post(
+        '/balance/change',
+        json={
+            'operation': OperationType.debit,
+            'user_id': first_user_id,
+            'total': 10,
+            'description': 'withdraw',
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+    await client.post(f'/account/create/user_id/{second_user_id}')
+    response = await client.post(
+        '/transactions/transfer',
+        json={
+            'sender_id': first_user_id,
+            'recipient_id': second_user_id,
+            'total': 40,
+            'description': 'payment',
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+    response = await client.get(f'/balance/get/user_id/{first_user_id}')
+    assert response.status_code == status.HTTP_200_OK
+    response = response.json()
+    assert response['balance'] == 50
+    response = await client.get(f'/balance/get/user_id/{second_user_id}')
+    assert response.status_code == status.HTTP_200_OK
+    response = response.json()
+    assert response['balance'] == 40
+
+    response = await client.get(f'/transactions/history/user_id/{first_user_id}')
+    assert response.status_code == status.HTTP_200_OK
+    response = response.json()['content']
+    assert response[0]['deal_with'] == first_user_id
+    assert response[0]['description'] == 'replenishment'
+    assert response[0]['total'] == 100
+    assert response[1]['deal_with'] == first_user_id
+    assert response[1]['description'] == 'withdraw'
+    assert response[1]['total'] == -10
+    assert response[2]['deal_with'] == second_user_id
+    assert response[2]['description'] == 'payment'
+    assert response[2]['total'] == -40
