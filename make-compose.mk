72d2a5e47a8e179fe7496d85165c48074e1c60b7
idilettant <dedenev.dev@gmail.com>
2021-12-01 20:33:17 +0300

    Create docker-compose Makefile

---
+++ make-compose.mk
@@ -0,0 +1,22 @@
+compose:
+	docker-compose up -d
+
+compose-production:
+	docker-compose --file docker-compose.yml run paymaster
+
+compose-build:
+	docker-compose build
+
+compose-logs:
+	docker-compose logs -f
+
+compose-down:
+	docker-compose down || true
+
+compose-stop:
+	docker-compose stop || true
+
+compose-restart:
+	docker-compose restart
+
+compose-setup: compose-down compose-build app-setup
