ce536f44d608a0e7195e613e1e648ce4377543e4
idilettant <dedenev.dev@gmail.com>
2021-12-02 19:05:57 +0300

    Add docuentation and refactor exceptions module

--- paymaster/exceptions.py
+++ paymaster/exceptions.py
@@ -3,19 +3,23 @@
 
 class PaymasterException(Exception):
     """Base app exception."""
+
     pass
 
 
 class AccountError(PaymasterException):
     """Exception of no account in database."""
+
     pass
 
 
 class CurrencyError(PaymasterException):
     """Exception of no currency in currencies table."""
+
     pass
 
 
 class BalanceValueError(PaymasterException):
     """Exception of negative account balance."""
+
     pass
