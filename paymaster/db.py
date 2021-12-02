f80599a63cf8fb0ab794ed464d75f65bb42645d4
idilettant <dedenev.dev@gmail.com>
2021-12-02 19:00:45 +0300

    Create documentation and refactor database module

--- paymaster/db.py
+++ paymaster/db.py
@@ -1,17 +1,28 @@
 """Database module."""
 from decimal import Decimal
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple
 
-import asyncpg
-from asyncpg import Connection
-from paymaster.data_schemas import OperationType
+from asyncpg import Connection, asyncpg
+from paymaster.data_schemas import OperationType, SortKey
 from paymaster.exceptions import AccountError, BalanceValueError, CurrencyError
 
 FRACTIONAL_VALUE = Decimal(100)
 
 
 async def create_acc(user_id: int, db_con: Connection) -> None:
-    query = """ INSERT INTO accounts (user_id) VALUES ($1);"""
+    """Create user account.
+
+    Args:
+        user_id: user id
+        db_con: connection to database
+
+    Raises:
+        AccountError: conflict with existiong account
+    """
+    query = """ INSERT INTO accounts (user_id)
+                VALUES ($1)
+                ON CONFLICT (user_id)
+                DO UPDATE SET status = 'active';"""
     if await _has_account(user_id, db_con):
         raise AccountError(f'Account with id <{user_id}> already exists')
     else:
@@ -19,25 +30,34 @@ async def create_acc(user_id: int, db_con: Connection) -> None:
 
 
 async def delete_acc(user_id: int, db_con: Connection) -> None:
-    tx_query = """  DELETE FROM transactions
-                    WHERE account_id = (
-                        SELECT id FROM accounts
-                        WHERE user_id = $1
-                    );"""
-    acc_query = """ DELETE FROM accounts
+    """Delete user account.
+
+    Args:
+        user_id: user id
+        db_con: connection to database
+    """
+    acc_query = """ UPDATE accounts
+                    SET status = 'deleted'
                     WHERE user_id = ($1);"""
-    async with db_con.transaction():
-        await db_con.execute(tx_query, user_id)
-        await db_con.execute(acc_query, user_id)
+    await db_con.execute(acc_query, user_id)
 
 
 async def change_balance(
-        user_id: int,
-        qty_value: Decimal,  # What if negative?
-        operation_type: str,
-        db_con: Connection,
-        description: Optional[str] = None,
+    user_id: int,
+    qty_value: Decimal,
+    operation_type: str,
+    db_con: Connection,
+    description: Optional[str] = None,
 ):
+    """Change user account balance.
+
+    Args:
+        user_id: user id
+        qty_value: quantity of transaction value
+        operation_type: type of transaction
+        db_con: connection to database
+        description: description of transaction aim
+    """
     if operation_type == OperationType.replenishment:
         await _crediting_balance(
             user_id=user_id,
@@ -55,53 +75,100 @@ async def change_balance(
 
 
 async def transfer_between_accs(
-        sender_id: int,
-        recipient_id: int,
-        qty_value: Decimal,
-        db_con: Connection,
-        description: Optional[str] = None,
+    sender_id: int,
+    recipient_id: int,
+    qty_value: Decimal,
+    db_con: Connection,
+    description: Optional[str] = None,
 ) -> None:
+    """Send funds from one account to another.
+
+    Args:
+        sender_id: sender user id
+        recipient_id: recipient user id
+        qty_value: quantity of transaction value
+        db_con: connection to database
+        description: description of transaction aim
+    """
     async with db_con.transaction():
         await _debiting_balance(
             user_id=sender_id,
             deal_with=recipient_id,
             qty_value=qty_value,
-            description='outcoming payment' if description is None else description,
+            description='outcoming payment' if description is None else description,  # noqa: E501
             db_con=db_con,
         )
         await _crediting_balance(
             user_id=recipient_id,
             deal_with=sender_id,
             qty_value=qty_value,
-            description='incoming payment' if description is None else description,
+            description='incoming payment' if description is None else description,  # noqa: E501
             db_con=db_con,
         )
 
 
-async def get_balance(user_id: int, db_con: Connection, convert_to: Optional[str] = None) -> Decimal:
+async def get_balance(
+    user_id: int,
+    db_con: Connection,
+    convert_to: Optional[str] = None,
+) -> Decimal:
+    """Get user account balance.
+
+    Args:
+        user_id: user id
+        db_con: database connection
+        convert_to: currency for convertation
+
+    Returns:
+        balance value
+
+    Raises:
+        AccountError: user account isn't registered
+    """
     if await _has_account(user_id, db_con):
         balance = await _compute_balance(user_id, db_con) / FRACTIONAL_VALUE
-        cur_rate = 1 if convert_to is None else await _fetch_currency_rate(cur_name=convert_to, db_con=db_con)
-        # Should I convert here or in app?
+        cur_rate = 1 if convert_to is None else await _fetch_currency_rate(
+            cur_name=convert_to,
+            db_con=db_con,
+        )
         return Decimal(round(balance * cur_rate, 2))
-    else:
-        raise AccountError(f'Has no registered account with id: {user_id}')
+    raise AccountError(f'Has no registered account with id: {user_id}')
 
 
-async def fetch_acc_history(
-        user_id: int,
-        db_con: Connection,
-        page_number: int = 1,
-        page_size: int = 20,
-        order_by: Optional[List[str]] = None,
+async def fetch_acc_history(  # noqa: WPS210 WPS211
+    user_id: int,
+    db_con: Connection,
+    page_number: int = 1,
+    page_size: int = 20,
+    order_by_date: Optional[SortKey] = None,
+    order_by_total: Optional[SortKey] = None,
 ) -> Tuple[Dict]:
-    order_by = ['-date'] if order_by is None else order_by
+    """Fetch user account transactions history.
+
+    Args:
+        user_id: user id
+        db_con: database connection
+        page_number: nuber of neccessary page
+        page_size: number of records per page
+        order_by_date: sort order by transaction date
+        order_by_total: sort order by transaction total value
+
+    Returns:
+        transactions history
+
+    Raises:
+        AccountError: user account isn't registered
+    """
     offset = (page_number - 1) * page_size
-    primary_key, additional_key, coma = await _get_sort_keys(order_by)
-    query: str = f"""   SELECT DATE(created_at) AS date, deal_with, description, qty_change as total
+    primary_key, additional_key, coma = await _get_sort_keys(order_by_date, order_by_total)  # noqa: E501
+    query: str = f"""   SELECT DATE(created_at)
+                            AS date, deal_with, description, qty_change as total
                             FROM transactions
                             WHERE account_id = (
-                                SELECT id FROM accounts WHERE user_id = $1
+                                SELECT id
+                                FROM accounts
+                                WHERE user_id = $1
+                                AND status = 'active'
                             )
                             ORDER BY {primary_key}{coma} {additional_key}
                             OFFSET $2 LIMIT $3;"""
@@ -109,11 +176,19 @@ async def fetch_acc_history(
 
     if history:
         return tuple(map(dict, history))
-    else:
-        raise AccountError(f'Has no registered account with id: {user_id}')
+    raise AccountError(f'Has no registered account with id: {user_id}')
 
 
-async def update_currencies(cur_rates: List[Tuple[Any]], db_con: Connection) -> None:  # TODO: make updating regular
+async def update_currencies(
+    cur_rates: List[Tuple[Any]],
+    db_con: Connection,
+) -> None:
+    """Update currencies rates table.
+
+    Args:
+        cur_rates: rates of currencies
+        db_con: darabase connection
+    """
     query = """ INSERT INTO currencies (cur_name, rate_to_base)
                 VALUES ($1, $2)
                 ON CONFLICT (cur_name)
@@ -123,11 +198,11 @@ async def update_currencies(cur_rates: List[Tuple[Any]], db_con: Connection) -> 
 
 
 async def _crediting_balance(
-        user_id: int,
-        qty_value: Decimal,
-        db_con: Connection,
-        deal_with: Optional[int] = None,
-        description: Optional[str] = None,
+    user_id: int,
+    qty_value: Decimal,
+    db_con: Connection,
+    deal_with: Optional[int] = None,
+    description: Optional[str] = None,
 ) -> None:
     deal_with = user_id if deal_with is None else deal_with
     description = 'replenishment' if description is None else description
@@ -136,11 +211,20 @@ async def _crediting_balance(
                     account_id, deal_with, description, qty_change
                 )
                 VALUES (
-                    (SELECT id FROM accounts WHERE user_id = $1), $2, $3, $4
+                    (SELECT id
+                    FROM accounts
+                    WHERE user_id = $1
+                    AND status = 'active'), $2, $3, $4
                 );"""
-    async with db_con.transaction():  # or privacy level?
+    async with db_con.transaction():
         try:
-            await db_con.execute(query, user_id, deal_with, description, fractional_qty_value)
+            await db_con.execute(
+                query,
+                user_id,
+                deal_with,
+                description,
+                fractional_qty_value,
+            )
         except asyncpg.exceptions.NotNullViolationError as exc:
             raise AccountError(
                 f'Has no registered account with id: {user_id}',
@@ -148,11 +232,11 @@ async def _crediting_balance(
 
 
 async def _debiting_balance(
-        user_id: int,
-        qty_value: Decimal,
-        db_con: Connection,
-        deal_with: Optional[int] = None,
-        description: Optional[str] = None,
+    user_id: int,
+    qty_value: Decimal,
+    db_con: Connection,
+    deal_with: Optional[int] = None,
+    description: Optional[str] = None,
 ) -> None:
     deal_with = user_id if deal_with is None else deal_with
     description = 'withdraw' if description is None else description
@@ -161,7 +245,10 @@ async def _debiting_balance(
                     account_id, deal_with, description, qty_change
                 )
                 VALUES (
-                    (SELECT id FROM accounts WHERE user_id = $1), $2, $3, $4
+                    (SELECT id
+                    FROM accounts
+                     WHERE user_id = $1
+                     AND status = 'active'), $2, $3, $4
                 );"""
     if not await _has_account(user_id, db_con):
         raise AccountError(f'Has no registered account with id: {user_id}')
@@ -169,9 +256,15 @@ async def _debiting_balance(
         balance = await _compute_balance(user_id, db_con)
         if balance - abs(fractional_qty_value) >= 0:
             try:
-                await db_con.execute(query, user_id, deal_with, description, -fractional_qty_value)  # Make quantity value negative
+                await db_con.execute(
+                    query,
+                    user_id,
+                    deal_with,
+                    description,
+                    -fractional_qty_value,  # Make quantity value negative
+                )
             except asyncpg.exceptions.ForeignKeyViolationError:
-                raise AccountError(f'Has no registered account with id: {deal_with}')
+                raise AccountError(f'Has no registered account with id: {deal_with}')  # noqa: E501
         else:
             raise BalanceValueError(
                 f'Insufficient funds on the account: {user_id}',
@@ -179,34 +272,46 @@ async def _debiting_balance(
 
 
 async def _fetch_currency_rate(cur_name: str, db_con: Connection) -> Decimal:
-    query = """ SELECT rate_to_base FROM currencies WHERE cur_name = $1;"""
+    query = """ SELECT rate_to_base
+                FROM currencies
+                WHERE cur_name = $1;"""
     rate = await db_con.fetchval(query, cur_name)
     if rate is None:
         raise CurrencyError(f'Unsupported currency type: {cur_name}')
-    else:
-        return Decimal(rate)
+    return Decimal(rate)
 
 
 async def _compute_balance(user_id: int, db_con: Connection) -> Decimal:
     balance_query = """ SELECT sum(qty_change)
                         FROM transactions
                         WHERE account_id = (
-                            SELECT id FROM accounts WHERE user_id = $1
+                            SELECT id
+                            FROM accounts
+                            WHERE user_id = $1
+                            AND status = 'active'
                         );"""
     balance = await db_con.fetchval(balance_query, user_id)
     return Decimal(0) if balance is None else Decimal(balance)
 
 
-async def _get_sort_keys(order_by: Union[str, List[str]]):
-    sort_keys = ['', '']
-    for key, val in enumerate(order_by):
-        column_name = val[1:] if val[0] == '-' else val
-        order = 'DESC' if val[0] == '-' else 'ASC'
-        sort_keys[key] = f'{column_name} {order}'
-    coma = ',' if sort_keys[1] else ''
-    return sort_keys[0], sort_keys[1], coma
+async def _get_sort_keys(order_by_date: SortKey, order_by_total: SortKey):
+    date_key = 'date'
+    total_key = 'total'
+    if order_by_date is None:
+        order_by_date = 'DESC'
+    else:
+        order_by_date = 'DESC' if order_by_date == SortKey.desc else 'ASC'
+    if order_by_total is not None:
+        order_by_total = 'DESC' if order_by_total == SortKey.desc else 'ASC'
+    coma = '' if order_by_total is None else ','
+    date_key = f'{date_key} {order_by_date}'
+    total_key = '' if order_by_total is None else f'{total_key} {order_by_total}'  # noqa: E501
+    return date_key, total_key, coma
 
 
 async def _has_account(user_id: int, db_con: Connection):
-    query = """ SELECT user_id FROM accounts WHERE user_id = $1;"""
+    query = """ SELECT user_id
+                FROM accounts
+                WHERE user_id = $1
+                AND status = 'active';"""
     return user_id == await db_con.fetchval(query, user_id)
