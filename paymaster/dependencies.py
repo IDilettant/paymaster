497354ac4c7df4cdee3de61e2971d5d8e1030467
idilettant <dedenev.dev@gmail.com>
2021-12-02 19:27:49 +0300

    Create documentation for dependencies module

--- paymaster/dependencies.py
+++ paymaster/dependencies.py
@@ -1,13 +1,30 @@
+"""Database dependencies for app."""
 from asyncpg import Connection, Pool
 from fastapi import Depends, Request
 
 
 def get_db_pool(request: Request) -> Pool:
+    """Extract database connections pool from app.
+
+    Args:
+        request: request containing application instance
+
+    Returns:
+        database connections pool
+    """
     return request.app.state.pool
 
 
 async def get_connection_from_pool(
-    pool: Pool = Depends(get_db_pool),
+    pool: Pool = Depends(get_db_pool),  # noqa: WPS404
 ) -> Connection:
+    """Get connection from database connections pool.
+
+    Args:
+        pool: database connections pool
+
+    Yields:
+        database connection
+    """
     async with pool.acquire() as conn:
         yield conn
