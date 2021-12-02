724e52a7954bc00c82ac8088201a7082038b7bd0
idilettant <dedenev.dev@gmail.com>
2021-12-02 19:20:21 +0300

    Create documentation for data schemas

--- paymaster/data_schemas.py
+++ paymaster/data_schemas.py
@@ -1,26 +1,39 @@
 """Responses and requests data schemas."""
 from decimal import Decimal
 from enum import Enum
-from typing import Any, Dict, List, Tuple
+from typing import Dict, Tuple
 
 from fastapi import status
 from paymaster.currencies import BASE_CURRENCY
 from pydantic import BaseModel, Field, PositiveInt, condecimal
 
 
-class OperationType(str, Enum):
+class OperationType(str, Enum):  # noqa: WPS600
+    """Operations types for transactions."""
+
     replenishment: str = 'replenishment'
     withdraw: str = 'withdraw'
 
 
+class SortKey(str, Enum):  # noqa: WPS600
+    """Sort order for sort keys."""
+
+    desc: str = 'desc'
+    asc: str = 'asc'
+
+
 class Balance(BaseModel):
-    status_code: int = Field(status.HTTP_200_OK, ge=100, lt=600)
+    """Response model for user balance."""
+
+    status_code: int = Field(status.HTTP_200_OK, ge=100, lt=600)  # noqa: WPS432
     user_id: PositiveInt
     balance: condecimal(decimal_places=2)
-    currency: str = Field(BASE_CURRENCY, min_length=3, max_length=3, description='')
+    currency: str = Field(BASE_CURRENCY, min_length=3, max_length=3)
 
 
 class Operation(BaseModel):
+    """Request model for change user balance."""
+
     operation: OperationType
     user_id: PositiveInt
     total: condecimal(gt=Decimal(0), decimal_places=2)
@@ -28,6 +41,8 @@ class Operation(BaseModel):
 
 
 class Transaction(BaseModel):
+    """Request model for transfer funds between user accounts."""
+
     sender_id: PositiveInt
     recipient_id: PositiveInt
     total: condecimal(gt=Decimal(0), decimal_places=2)
@@ -35,5 +50,7 @@ class Transaction(BaseModel):
 
 
 class PageOut(BaseModel):
-    status_code: int = Field(status.HTTP_200_OK, ge=100, lt=600)
-    content: Tuple[Dict, ...]
+    """Response model for user account transactions history request."""
+
+    status_code: int = Field(status.HTTP_200_OK, ge=100, lt=600)  # noqa: WPS432
+    content: Tuple[Dict, ...]  # noqa: WPS110
