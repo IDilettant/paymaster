ad466f80355ffa89d02a4d16d7fa40d16cb39359
idilettant <dedenev.dev@gmail.com>
2021-12-03 13:32:55 +0300

    Refactor minor typos

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 """Fixtures module."""
 import os
+from typing import AsyncIterator
 
 import pytest
 from asgi_lifespan import LifespanManager
@@ -12,11 +13,12 @@ from testcontainers.postgres import PostgresContainer
 @pytest.fixture
 async def dsn() -> Pool:
     with PostgresContainer("postgres:12-alpine") as postgres:
-        yield postgres.get_connection_url().replace('+psycopg2', '')  # Delete from dsn url unnecessary path part
+        # delete from db url unnecessary path part
+        yield postgres.get_connection_url().replace('+psycopg2', '')
 
 
 @pytest.fixture
-async def app() -> FastAPI:
+async def app() -> AsyncIterator[FastAPI]:
     from paymaster.main import \
         get_application  # local import for testing purpose
 
@@ -24,14 +26,14 @@ async def app() -> FastAPI:
 
 
 @pytest.fixture
-async def initialized_app(app: FastAPI, dsn: str) -> FastAPI:
+async def initialized_app(app: FastAPI, dsn: str) -> AsyncIterator[FastAPI]:
     os.environ['DSN'] = dsn
     async with LifespanManager(app):
         yield app
 
 
 @pytest.fixture
-async def client(initialized_app: FastAPI) -> AsyncClient:
+async def client(initialized_app: FastAPI) -> AsyncIterator[AsyncClient]:
     async with AsyncClient(
         app=initialized_app,
         base_url="http://testserver",
