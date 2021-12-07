b9aae63d6a6247d3bf8e351f078b77051d21a79f
idilettant <dedenev.dev@gmail.com>
2021-12-07 17:29:27 +0300

    Refactor queries with updated tables accordance

--- paymaster/database/db.py
+++ paymaster/database/db.py
@@ -20,16 +20,11 @@ async def create_acc(user_id: int, db_con: Connection) -> None:
         AccountError: conflict with existiong account
     """
     query = """ INSERT INTO accounts (user_id)
-                VALUES ($1)
-                ON CONFLICT (user_id)
-                DO UPDATE SET status = 'active';"""
-    # FIXME: я бы не смешивал «реактвиацию аккаунта» и создание нового, потому что это принципиально разные операции. У юзера может заблокироваться один счет и создаться новый, это норм.
-    # FIXME: зачем проверка has_account, если можно обработать on conflict при дубликате ключа?
-    # FIXME: если несколько запросов оставляем, то нужна транзакция
-    if await _has_account(user_id, db_con):
-        raise AccountError(f'Account with id <{user_id}> already exists')
-    else:
+                VALUES ($1);"""
+    try:
         await db_con.execute(query, user_id)
+    except exceptions.UniqueViolationError:
+        raise AccountError(f'Account with id <{user_id}> already exists')
 
 
 async def delete_acc(user_id: int, db_con: Connection) -> None:
@@ -43,15 +38,12 @@ async def delete_acc(user_id: int, db_con: Connection) -> None:
         AccountError: conflict with non existiong account
     """
     acc_query = """ UPDATE accounts
-                    SET status = 'deleted'
-                    WHERE user_id = ($1);"""
-    # FIXME: если несколько запросов, то нужна транзакция
-    if await _has_account(user_id, db_con):
-        # FIXME: не нужно проверять, чтобы узнать, что такого нет
-        # можно по результату execute проверить, была ли изменена хотя бы одна строка
-        # https://magicstack.github.io/asyncpg/current/api/index.html#asyncpg.connection.Connection.execute
-        await db_con.execute(acc_query, user_id)
-    else:
+                        SET current_status = 'deleted'
+                        WHERE user_id = ($1)
+                        AND current_status = 'active';"""
+    executing_status = await db_con.execute(acc_query, user_id)
+    changed_row_num = int(executing_status.split()[-1])
+    if changed_row_num == 0:
         raise AccountError(f"Account with id <{user_id}> don't exists")
 
 
@@ -175,14 +167,19 @@ async def fetch_acc_history(  # noqa: WPS210 WPS211
     """
     offset = (page_number - 1) * page_size
     primary_key, additional_key, coma = await _get_sort_keys(order_by_date, order_by_total)  # noqa: E501
-    query: str = f"""   SELECT DATE(created_at)
-                            AS date, deal_with, description, qty_change as total
+    query: str = f"""   SELECT 
+                            DATE(created_at) AS date, 
+                            (SELECT user_id
+                                FROM accounts
+                                WHERE id = deal_with) AS deal_with, 
+                            description, 
+                            qty_change AS total
                             FROM transactions
                             WHERE account_id = (
                                 SELECT id
                                 FROM accounts
                                 WHERE user_id = $1
-                                AND status = 'active'
+                                AND current_status = 'active'
                             )
                             ORDER BY {primary_key}{coma} {additional_key}
                             OFFSET $2 LIMIT $3;"""
@@ -205,10 +202,10 @@ async def update_currencies(
         db_con: darabase connection
     """
     query = """ INSERT INTO currencies (cur_name, rate_to_base)
-                VALUES ($1, $2)
-                ON CONFLICT (cur_name)
-                DO UPDATE SET rate_to_base = $2
-                WHERE currencies.cur_name = $1;"""
+                    VALUES ($1, $2)
+                    ON CONFLICT (cur_name)
+                    DO UPDATE SET rate_to_base = $2
+                    WHERE currencies.cur_name = $1;"""
     await db_con.executemany(query, cur_rates)
 
 
@@ -228,9 +225,14 @@ async def _crediting_balance(
                 )
                 VALUES (
                     (SELECT id
-                    FROM accounts
-                    WHERE user_id = $1
-                    AND status = 'active'), $2, $3, $4
+                        FROM accounts
+                        WHERE user_id = $1
+                        AND current_status = 'active'), 
+                    (SELECT id
+                        FROM accounts
+                        WHERE user_id = $2
+                        AND current_status = 'active'),
+                    $3, $4
                 );"""
     async with db_con.transaction():
         try:
@@ -265,9 +267,14 @@ async def _debiting_balance(
                 )
                 VALUES (
                     (SELECT id
-                    FROM accounts
-                     WHERE user_id = $1
-                     AND status = 'active'), $2, $3, $4
+                        FROM accounts
+                        WHERE user_id = $1
+                        AND current_status = 'active'), 
+                    (SELECT id
+                        FROM accounts
+                        WHERE user_id = $2
+                        AND current_status = 'active'),
+                    $3, $4
                 );"""
     # FIXME: зачем явно проверять, если можно просто словить ошибку в транзакции как на 28 строчке?
     # если несколько запросов, то нужна транзакция
@@ -284,7 +291,7 @@ async def _debiting_balance(
                     description,
                     -fractional_qty_value,  # Make quantity value negative
                 )
-            except exceptions.ForeignKeyViolationError:
+            except exceptions.NotNullViolationError:
                 raise AccountError(f'Has no registered account with id: {deal_with}')  # noqa: E501
         else:
             raise BalanceValueError(
@@ -304,13 +311,13 @@ async def _fetch_currency_rate(cur_name: str, db_con: Connection) -> Decimal:
 
 async def _compute_balance(user_id: int, db_con: Connection) -> Decimal:
     balance_query = """ SELECT sum(qty_change)
-                        FROM transactions
-                        WHERE account_id = (
-                            SELECT id
-                            FROM accounts
-                            WHERE user_id = $1
-                            AND status = 'active'
-                        );"""
+                            FROM transactions
+                            WHERE account_id = (
+                                SELECT id
+                                FROM accounts
+                                WHERE user_id = $1
+                                AND current_status = 'active'
+                            );"""
     balance = await db_con.fetchval(balance_query, user_id)
     return Decimal(0) if balance is None else Decimal(balance)
 
@@ -342,5 +349,5 @@ async def _has_account(user_id: int, db_con: Connection):
     query = """ SELECT user_id
                 FROM accounts
                 WHERE user_id = $1
-                AND status = 'active';"""
+                AND current_status = 'active';"""
     return user_id == await db_con.fetchval(query, user_id)
