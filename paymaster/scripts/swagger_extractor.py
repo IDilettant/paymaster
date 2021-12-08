0452b1eb52516e51359042448dab786d120bac4e
idilettant <dedenev.dev@gmail.com>
2021-12-08 15:10:57 +0300

    Reafactor way cript calling

--- paymaster/scripts/swagger_extractor.py
+++ paymaster/scripts/swagger_extractor.py
@@ -2,13 +2,7 @@
 import yaml
 from paymaster.app.main import app
 
-
-def main():
-    """Extract openapi documentation from app."""
+if __name__ == '__main__':
     openapi_data = app.openapi()
     with open('doc/openapi.yml', 'w') as file:  # noqa: WPS110
         yaml.dump(openapi_data, file)
-
-
-if __name__ == '__main__':
-    main()
