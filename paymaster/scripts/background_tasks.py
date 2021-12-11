bea5595b6c4c41972299dfb870cb8f550a880cf5
idilettant <dedenev.dev@gmail.com>
2021-12-11 17:57:16 +0300

    Create script for backround currencies rate updationg

--- paymaster/scripts/background_tasks.py
+++ paymaster/scripts/background_tasks.py
@@ -1,15 +1,13 @@
+"""Background tasks module."""
 import asyncio
 import functools
 import logging
 import os
-import threading
 import time
-from datetime import timedelta, datetime
-from typing import Any, Callable, Coroutine, Optional
+from typing import Any, Callable, Optional
 
-import freezegun as freezegun
 import schedule
-from asyncpg import Pool, create_pool
+from asyncpg import Connection, connect
 from dotenv import load_dotenv
 from paymaster.currencies import get_currencies_rates
 from paymaster.database.db import update_currencies
@@ -18,25 +16,15 @@ from paymaster.exceptions import CurrencyError
 LOGGER = logging.getLogger('schedule')
 LOGGER.setLevel(level=logging.DEBUG)
 
+load_dotenv()
 
-def run_continuously(interval=1):
-    cease_continuous_run = threading.Event()
-
-    class ScheduleThread(threading.Thread):
-        @classmethod
-        def run(cls):
-            while not cease_continuous_run.is_set():
-                schedule.run_pending()
-                time.sleep(interval)
-
-    continuous_thread = ScheduleThread()
-    continuous_thread.start()
-    return cease_continuous_run
+API_KEY: Optional[str] = os.getenv('API_KEY')
+DSN: Optional[str] = os.getenv('DSN')
 
 
 def catch_exceptions(  # noqa: WPS234
     cancel_on_failure: bool = False,
-) -> Callable[[Callable[[Any, Optional[str]], Coroutine[Any, Any, Any]]], Any]:  # noqa: WPS221 E501
+) -> Callable[[Callable[[Callable[[Any, Any], None]], Any]], Any]:  # noqa: WPS221 E501
     """Make decorator for cathing exceptions in background scheduler.
 
     Args:
@@ -46,13 +34,13 @@ def catch_exceptions(  # noqa: WPS234
         catch exceptions decorator
     """
     def catch_exceptions_decorator(  # noqa: WPS430
-        job_func: Callable[[Any, Optional[str]], Coroutine[Any, Any, Any]],  # noqa: WPS221 E501
+        job_func: Callable[[Callable[[Any, Any], None]], Any],  # noqa: WPS221 E501
     ):
         @functools.wraps(job_func)
         def wrapper(*args):
             try:
                 return job_func(*args)
-            except CurrencyError as exc:
+            except (CurrencyError, RuntimeError) as exc:
                 LOGGER.warning(exc)
                 if cancel_on_failure:
                     return schedule.CancelJob
@@ -60,34 +48,31 @@ def catch_exceptions(  # noqa: WPS234
     return catch_exceptions_decorator
 
 
-async def update_data_currencies(pool: Pool, api_key: Optional[str]):  # noqa: D103 E501
+async def fetch_and_update_data_currencies(db_conn: Connection, api_key: Optional[str]) -> None:  # noqa: D103 E501
     cur_rates = await get_currencies_rates(api_key)
-    async with pool.acquire() as db_conn:
-        await update_currencies(cur_rates, db_conn)
+    await update_currencies(cur_rates, db_conn)
 
 
-async def run_background_job() -> None:
-    load_dotenv()
-    dsn: Optional[str] = os.getenv('DSN')
-    api_key: Optional[str] = os.getenv('API_KEY')
-    pool = await create_pool(dsn)
-    await update_data_currencies(pool, api_key)
-    print('Done')
+async def _run_background_job() -> None:
+    db_conn = await connect(DSN)
+    await fetch_and_update_data_currencies(db_conn, API_KEY)
 
 
-@catch_exceptions(cancel_on_failure=True)
-def set_task(task, trigger_time: str):
+@catch_exceptions(cancel_on_failure=True)  # type: ignore
+def _set_task(
+    task,
+    trigger_time,
+) -> None:
     schedule.every().day.at(trigger_time).do(
         asyncio.run,
         task(),
     )
-    run_continuously()
+    while True:  # noqa: WPS457
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
-    now = datetime(2020, 1, 1, 10, 31)
-    with freezegun.freeze_time(now) as frozen_date:
-        set_task(run_background_job, '10:30')
-        frozen_date.move_to(now + timedelta(days=1))
-        time.sleep(5)
-        print('Now')
+    connection = await connect(DSN)
+    fetch_and_update_data_currencies(db_conn=connection, api_key=API_KEY)
+    _set_task(_run_background_job, '12:31')
